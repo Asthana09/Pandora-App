6d36ab90862f9a772e690982ece2182d94707feb
Deepti Asthana <neenaasthana20@gmail.com>
2024-03-26 17:50:08 +0530

    e-commoerce-app

--- src/app/logIn/page.js
+++ src/app/logIn/page.js
@@ -1,11 +1,46 @@
+"use client"
+// import Link from 'next/link'
 import React from 'react'
+import {useState} from 'react'
+
+
+
 
 const SignIn = () => {
+  const [name , setName] = useState("")
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+
   return (
-    <div>
-        <h2>Sign in</h2>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h2>logIn</h2>
+
+      {/* <label className='text-left' htmlFor='name'>Name</label> */}
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+       type='text' placeholder='Name' 
+      value={name}  
+      onChange={(e)=>setName(e.target.value)}
+      />
+
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='email' placeholder='Email' 
+      value={email}  
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+      
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='password' placeholder='Password' 
+      value={password}  
+      onChange={(e)=>setPassword(e.target.value)}
+      />
+        
+        <button className='btn waves-effect waves-light' type='submit'>SignIn</button>
+      
+       
     </div>
   )
 }
+
+
 
 export default SignIn
--- src/app/registration/page.js
+++ src/app/registration/page.js
@@ -1,15 +1,65 @@
-import Link from 'next/link'
+ "use client"
+// import Link from 'next/link'
 import React from 'react'
+import {useState} from 'react'
+
+
 
 
 const Registration = () => {
-  return (
-    <div>
-        <h1 className='text-blue-900 font-bold text-2xl' >
-           <p>Sign Up Here</p>  
-            <Link href="/logIn" className='font-bold '>If already registered - Sign In</Link>
+  const [name , setName] = useState("")
+  const [email , setEmail] = useState("")
+  const [phone , setPhone] = useState("")
+  const [address , setAddress] = useState("")
+  const [pincode , setPincode] = useState("")
+  const [password , setPassword] = useState("")
 
-        </h1>
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h2>Register</h2>
+
+      {/* <label className='text-left' htmlFor='name'>Name</label> */}
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+       type='text' placeholder='Name' 
+      value={name}  
+      onChange={(e)=>setName(e.target.value)}
+      />
+
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='email' placeholder='Email' 
+      value={email}  
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='number' placeholder='Mobile Number' 
+      value={phone}  
+      onChange={(e)=>setPhone(e.target.value)}
+      />
+
+
+    <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700' 
+     type='text' placeholder='Address' 
+      value={address}  
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='number' placeholder='Pin' 
+      value={pincode}  
+      onChange={(e)=>setPincode(e.target.value)}
+      />
+      
+      <input className='p-2 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-700'
+      type='password' placeholder='Password' 
+      value={password}  
+      onChange={(e)=>setPassword(e.target.value)}
+      />
+        
+        <button className='btn waves-effect waves-light' type='submit'>SignUp</button>
+      
+       
+        {/* <link href="/logIn"><a><h5>If already Registered</h5></a></link> */}
     </div>
   )
 }
